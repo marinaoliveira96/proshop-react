84baef237b2a48b52b443d60d2941b30d0653328
Marina Oliveira <marina09oliveira@gmail.com>
2020-11-01 14:02:33 -0300

    Generate a JSON Web Token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 import asyncHandler from 'express-async-handler';
 
 // @desc  Auth user & get token
@@ -15,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
